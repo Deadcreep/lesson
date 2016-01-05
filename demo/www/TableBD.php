6cb5362717d3ec05785a77d7bbec310b9e68706b
Deadcreep <tolla.von@gmail.com>
2016-01-05 11:21:53 +0500

    Add lesson

---
+++ demo/www/TableBD.php
@@ -0,0 +1,40 @@
+﻿<?php 
+     mb_internal_encoding("utf-8");
+    $db = mysql_connect("localhost","localhost","123");
+    mysql_select_db("name", $db);
+    $result = mysql_query("SELECT * FROM names", $db);
+         
+?>
+
+<!DOCTYPE html>
+
+<html lang="ru">    
+    <head>
+    	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+        <title>Table</title>
+        <link rel='stylesheet' href='css/bootstrap.min.css' type='text/css' media='all'>
+               <link rel='stylesheet' href='/css/creative.css' media='all'>
+    </head>
+    <body>    	
+        <div id="table">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr class="success">
+                  <th>Номер</th>
+                  <th>Имя</th>          
+                </tr>
+              </thead>
+              <tbody>
+                  <?php mb_internal_encoding("UTF-8");
+						while ($row = mysql_fetch_assoc($result)) {
+							?>
+							<tr>
+                        <td><?php echo $row['ID']; ?></td>
+                        <td><?php echo $row['name'] ?></td>
+                    </tr> 			    	
+                    <?php } ?>
+              </tbody>
+            </table>
+        </div>        
+    </body>
+</html>
---
+++ demo/www/TableBootstrap.php
@@ -0,0 +1,34 @@
+﻿<?php 
+    $arr = Array('1' => 'Антоний', 'Гай', 'Тиберий', 'Юлий', 'Марк', 'Корнелий', 'Брут', 'Троян', 'Аврелий');     
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">    
+    <head>
+        <title>Table</title>
+        <link rel='stylesheet' href='css/bootstrap.min.css' type='text/css' media='all'>
+               <link rel='stylesheet' href='/css/creative.css' media='all'>
+    </head>
+    <body>
+        <div id="table">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr class="success">
+                  <th>Номер</th>
+                  <th>Имя</th>          
+                </tr>
+              </thead>
+              <tbody>
+                  <?php foreach(array_keys($arr) as $key): ?>
+                    <tr>
+                        <td><?php echo $key; ?></td>
+                        <td><?php echo $arr[$key] ?></td>
+                    </tr>
+                  <?php endforeach;?>
+              </tbody>
+            </table>
+        </div>       
+    </body>
+</html>
+
