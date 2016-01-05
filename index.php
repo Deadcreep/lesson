4f6732691c03823580d9c5f9d27a46c585238580
Deadcreep <tolla.von@gmail.com>
2016-01-05 11:16:45 +0500

    Init

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <link rel='stylesheet' href='/css/bootstrap.min.css' type='text/css' media='all'>
+        <link rel='stylesheet' href='/css/creative.css' media='all'>        
+        <title>Table Bootstrap</title>
+    </head>
+    <body>
+        <div id="table-button">
+            <form action="TableBootstrap.php">
+              <button type="submit" class="btn btn-info">Таблица с foreach</button>              
+            </form>
+        </div>
+        <div id="tableBD-button">
+            <form action="TableBD.php">
+              <button type="submit" class="btn btn-info">Таблица из БД</button>              
+            </form>
+        </div>
+    </body>
+</html>
+
